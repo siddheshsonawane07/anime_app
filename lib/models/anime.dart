6587ee3a9c72ad5846dea52a20efc425c65da367
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-03-31 16:45:03 +0530

    assets downloaded

--- lib/models/anime.dart
+++ lib/models/anime.dart
@@ -13,9 +13,17 @@ class Anime {
 }
 
 List<Anime> trendsData = [
-  Anime(name: 's', number: 5, poster: "5", score: 6),
-  Anime(name: 's', number: 5, poster: "5", score: 6),
-  Anime(name: 's', number: 5, poster: "5", score: 6),
-  Anime(name: 's', number: 5, poster: "5", score: 6),
-  Anime(name: 's', number: 5, poster: "5", score: 6),
+  Anime(name: 'Naruto Shippuden', number: 1, poster: 'assets/images/naruto_shippuden.jpg', score: 8),
+  Anime(name: 'Naruto ', number: 2, poster: 'assets/images/naruto.jpg', score: 7),
+  Anime(name: 'Death Note', number: 3, poster: 'assets/images/deathnote.jpg', score: 8),
+  Anime(name: 'Erased', number: 4, poster: 'assets/images/erased.jpg', score: 9),
+  Anime(name: 'Attack on Titan', number: 5, poster: 'assets/images/aot.jpg', score: 9),
+];
+
+List<Anime> recentsData = [
+  Anime(name: 'Rezero', number: 1, poster: 'assets/images/rezero.jpg', score: 8),
+  Anime(name: 'Dr. Stone ', number: 2, poster: 'assets/images/dr.stone.png', score: 7),
+  Anime(name: 'One Piece', number: 3, poster: 'assets/images/onepiece.jpg', score: 8),
+  Anime(name: 'Tokyo Ghoul', number: 4, poster: 'assets/images/tokyo.jpg', score: 9),
+  Anime(name: 'Steins Gate', number: 5, poster: 'assets/images/steins.jpg', score: 9),
 ];
